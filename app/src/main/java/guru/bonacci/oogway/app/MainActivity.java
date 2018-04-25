7294ed629cd5e733a6c3bf54ee7f4b934e272086
WhoAm2 <jvh1234@outlook.com>
2018-04-25 13:37:49 +0200

    call spring demo json

--- app/src/main/java/guru/bonacci/oogway/app/MainActivity.java
+++ app/src/main/java/guru/bonacci/oogway/app/MainActivity.java
@@ -14,6 +14,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.Response;
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -22,20 +23,17 @@ import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etGitHubUser; // This will be a reference to our GitHub username input.
     Button btnGetRepos;  // This is a reference to the "Get Repos" button.
     TextView tvRepoList;  // This will reference our repo list text box.
     RequestQueue requestQueue;  // This is our requests queue to process our HTTP requests.
 
-    String baseUrl = "https://api.github.com/users/";  // This is the API base URL (GitHub API)
-    String url; // This will hold the full URL which will include the username entered in the etGitHubUser.
+    String url = "http://rest-service.guides.spring.io/greeting";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);  // This is some magic for Android to load a previously saved state for when you are switching between actvities.
         setContentView(R.layout.activity_main);  // This links our code to our layout which we defined earlier.
 
-        this.etGitHubUser = (EditText) findViewById(R.id.et_github_user);  // Link our github user text box.
         this.btnGetRepos = (Button) findViewById(R.id.btn_get_repos);  // Link our clicky button.
         this.tvRepoList = (TextView) findViewById(R.id.tv_repo_list);  // Link our repository list text output box.
         this.tvRepoList.setMovementMethod(new ScrollingMovementMethod());  // This makes our text box scrollable, for those big GitHub contributors with lots of repos :)
@@ -63,54 +61,33 @@ public class MainActivity extends AppCompatActivity {
         this.tvRepoList.setText(str);
     }
 
-    private void getRepoList(String username) {
-        // First, we insert the username into the repo url.
-        // The repo url is defined in GitHubs API docs (https://developer.github.com/v3/repos/).
-        this.url = this.baseUrl + username + "/repos";
+    private void getRepoList() {
+        JsonObjectRequest request = new JsonObjectRequest
+                (Request.Method.GET, url, (String)null, new Response.Listener<JSONObject>() {
 
-        // Next, we create a new JsonArrayRequest. This will use Volley to make a HTTP request
-        // that expects a JSON Array Response.
-        // To fully understand this, I'd recommend readng the office docs: https://developer.android.com/training/volley/index.html
-        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, url,
-                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(JSONArray response) {
-                        // Check the length of our response (to see if the user has any repos)
-                        if (response.length() > 0) {
-                            // The user does have repos, so let's loop through them all.
-                            for (int i = 0; i < response.length(); i++) {
-                                try {
-                                    // For each repo, add a new line to our repo list.
-                                    JSONObject jsonObj = response.getJSONObject(i);
-                                    String repoName = jsonObj.get("name").toString();
-                                    String lastUpdated = jsonObj.get("updated_at").toString();
-                                    addToRepoList(repoName, lastUpdated);
-                                } catch (JSONException e) {
-                                    // If there is an error then output this to the logs.
-                                    Log.e("Volley", "Invalid JSON Object.");
-                                }
-
-                            }
-                        } else {
-                            // The user didn't have any repos.
-                            setRepoListText("No repos found.");
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String id = response.getString("id");
+                            String content = response.getString("content");
+                            addToRepoList(id, content);
+                        } catch (JSONException e) {
+                            // If there is an error then output this to the logs.
+                            Log.e("Volley", "Invalid JSON Object.");
                         }
-
                     }
-                },
+                }, new Response.ErrorListener() {
 
-                new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // If there a HTTP error then add a note to our repo list.
-                        setRepoListText("Error while calling REST API");
-                        Log.e("Volley", error.toString());
+                        // TODO: Handle error
+
                     }
-                }
-        );
+                });
+
         // Add the request we just defined to our request queue.
         // The request queue will automatically handle the request as soon as it can.
-        requestQueue.add(arrReq);
+        requestQueue.add(request);
     }
 
     public void getReposClicked(View v) {
@@ -118,6 +95,6 @@ public class MainActivity extends AppCompatActivity {
         clearRepoList();
         // Call our getRepoList() function that is defined above and pass in the
         // text which has been entered into the etGitHubUser text input field.
-        getRepoList(etGitHubUser.getText().toString());
+        getRepoList();
     }
 }
