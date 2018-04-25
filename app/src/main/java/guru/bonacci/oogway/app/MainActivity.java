a60cb275ea87d4f2dc002296744fd29d149fa6ec
WhoAm2 <jvh1234@outlook.com>
2018-04-25 13:50:38 +0200

    finished: rest service calling app

--- app/src/main/java/guru/bonacci/oogway/app/MainActivity.java
+++ app/src/main/java/guru/bonacci/oogway/app/MainActivity.java
@@ -1,67 +1,54 @@
 package guru.bonacci.oogway.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.Response;
 import com.android.volley.Request;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnGetRepos;  // This is a reference to the "Get Repos" button.
-    TextView tvRepoList;  // This will reference our repo list text box.
-    RequestQueue requestQueue;  // This is our requests queue to process our HTTP requests.
+    Button btnAsk;
+    TextView tvAnswer;
+    RequestQueue requestQueue;
 
     String url = "http://rest-service.guides.spring.io/greeting";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);  // This is some magic for Android to load a previously saved state for when you are switching between actvities.
-        setContentView(R.layout.activity_main);  // This links our code to our layout which we defined earlier.
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        this.btnGetRepos = (Button) findViewById(R.id.btn_get_repos);  // Link our clicky button.
-        this.tvRepoList = (TextView) findViewById(R.id.tv_repo_list);  // Link our repository list text output box.
-        this.tvRepoList.setMovementMethod(new ScrollingMovementMethod());  // This makes our text box scrollable, for those big GitHub contributors with lots of repos :)
+        this.btnAsk = (Button) findViewById(R.id.btn_ask);
+        this.tvAnswer = (TextView) findViewById(R.id.tv_answer);
 
-        requestQueue = Volley.newRequestQueue(this);  // This setups up a new request queue which we will need to make HTTP requests.
+        requestQueue = Volley.newRequestQueue(this);
     }
 
-    private void clearRepoList() {
-        // This will clear the repo list (set it as a blank string).
-        this.tvRepoList.setText("");
+    private void clearAnswer() {
+        this.tvAnswer.setText("");
     }
 
-    private void addToRepoList(String repoName, String lastUpdated) {
-        // This will add a new repo to our list.
-        // It combines the repoName and lastUpdated strings together.
-        // And then adds them followed by a new line (\n\n make two new lines).
-        String strRow = repoName + " / " + lastUpdated;
-        String currentText = tvRepoList.getText().toString();
-        this.tvRepoList.setText(currentText + "\n\n" + strRow);
+    private void setAnswer(String id, String content) {
+        this.tvAnswer.setText("\n\n\n\n\n" + id + " / " + content);
     }
 
-    private void setRepoListText(String str) {
-        // This is used for setting the text of our repo list box to a specific string.
-        // We will use this to write a "No repos found" message if the user doens't have any.
-        this.tvRepoList.setText(str);
+    private void setAnswer(String str) {
+        this.tvAnswer.setText(str);
     }
 
-    private void getRepoList() {
+    private void getAnswer() {
         JsonObjectRequest request = new JsonObjectRequest
                 (Request.Method.GET, url, (String)null, new Response.Listener<JSONObject>() {
 
@@ -70,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             String id = response.getString("id");
                             String content = response.getString("content");
-                            addToRepoList(id, content);
+                            setAnswer(id, content);
                         } catch (JSONException e) {
                             // If there is an error then output this to the logs.
                             Log.e("Volley", "Invalid JSON Object.");
@@ -80,8 +67,8 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-
+                        setAnswer("Error while calling REST API");
+                        Log.e("Volley", error.toString());
                     }
                 });
 
@@ -90,11 +77,8 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.add(request);
     }
 
-    public void getReposClicked(View v) {
-        // Clear the repo list (so we have a fresh screen to add to)
-        clearRepoList();
-        // Call our getRepoList() function that is defined above and pass in the
-        // text which has been entered into the etGitHubUser text input field.
-        getRepoList();
+    public void getAnswer(View v) {
+        clearAnswer();
+        getAnswer();
     }
 }
