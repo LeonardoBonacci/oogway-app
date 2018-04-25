24ce8b648b7fbf3aae3d4ba7d9211fd8ecd9f527
WhoAm2 <jvh1234@outlook.com>
2018-04-25 19:09:44 +0200

    oogway-app speaks

--- app/src/main/java/guru/bonacci/oogway/app/MainActivity.java
+++ app/src/main/java/guru/bonacci/oogway/app/MainActivity.java
@@ -1,11 +1,13 @@
 package guru.bonacci.oogway.app;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -17,11 +19,14 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnAsk;
     TextView tvAnswer;
     RequestQueue requestQueue;
+    TextToSpeech t1;
 
     String url = "http://rest-service.guides.spring.io/greeting";
 
@@ -34,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
         this.tvAnswer = (TextView) findViewById(R.id.tv_answer);
 
         requestQueue = Volley.newRequestQueue(this);
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
     }
 
     private void clearAnswer() {
@@ -42,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void setAnswer(String id, String content) {
         this.tvAnswer.setText("\n\n\n\n\n" + id + " / " + content);
+
+        String toSpeak = "And those who were seen dancing were thought to be insane by those who could not hear the music.";
+        Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+        t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
     }
 
     private void setAnswer(String str) {
